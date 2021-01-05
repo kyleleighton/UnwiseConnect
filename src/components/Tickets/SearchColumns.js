1b7cf1016c1d8927f304718cc8a5b758325151e3
Kyle Leighton <kyle.leighton1@gmail.com>
2021-01-05 01:15:13 -0500

    Add more

--- src/components/Tickets/SearchColumns.js
+++ src/components/Tickets/SearchColumns.js
@@ -2,6 +2,8 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Select from 'react-select';
 import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
 
 class SearchColumns extends React.Component {
   state = {
@@ -22,11 +24,7 @@ class SearchColumns extends React.Component {
         dataField: column.property,
         text: column.header.label,
         sort: true,
-        sortCaret: (order, column) => {
-          if (!order || order === 'asc') return (<span className="sorting-arrow">Up</span>);
-          else if (order === 'desc') return (<span className="sorting-arrow down-arrow">Down</span>);
-          return null;
-        },
+        filter: textFilter()
       })
     });
     this.setState({
@@ -58,81 +56,20 @@ class SearchColumns extends React.Component {
   render() {
     return (
       <React.Fragment>
-        {/* {this.state.columns !== [] && (
-          <BootstrapTable keyField='id' data={ this.state.rows } columns={ this.state.columns } />
-        )} */}
-
+        {this.state.columns.length > 0 && (
+          <BootstrapTable pagination={ paginationFactory()} filter={ filterFactory()} classes="table table-striped table-bordered" keyField='id' data={ this.state.rows } columns={ this.state.columns } />
+        )}
+        {/* <tr>
+          {this.props.columns.map((column, i) => (
+            <th key={`${column.property || i}-column-filter`} className="column-filter">
+              {column && column.property ?
+                this.renderFilter(column)
+              : ''}
+            </th>
+          ))}
+        </tr> */}
       </React.Fragment>
     );
-  }
-
-  renderFilter(column) {
-    if (column.filterType === 'dropdown') {
-      return this.renderDropdownFilter(column);
-    } else if (column.filterType === 'none') {
-      return '';
-    } else if (column.filterType === 'custom') {
-      return typeof column.customFilter === 'function' ? column.customFilter() : column.customFilter;
-    }
-    return this.renderTextFilter(column);
-  }
-
-  renderTextFilter(column) {
-    const onQueryChange = (event) => {
-      this.props.onChange({
-        ...this.props.query,
-        [event.target.name]: event.target.value,
-      });
-    };
-
-    return (
-      <input
-        autoComplete="off"
-        onChange={onQueryChange}
-        className="column-filter-input"
-        name={column.property}
-        placeholder={column.filterPlaceholder || ''}
-        value={this.props.query[column.property] || ''}
-      />
-    );
-  }
-
-  renderDropdownFilter(column) {
-    const onQueryChange = (values) => {
-      // Flatten subarrays.
-      const flatValues = values.map(option => option.value).reduce((acc, val) => acc.concat(Array.isArray(val) ? val : [val]), []);
-      this.props.onChange({
-        ...this.props.query,
-        [column.property]: flatValues.length ? [ ...new Set(flatValues) ] : '',
-      });
-    };
-
-    const rowValues = this.props.rows.map(row => row[column.property]);
-    const uniqueRowValues = [ ...new Set(rowValues) ];
-    const extraOptions = (column.extraOptions || []).map(this.evaluateExtraOption.bind(this, column, uniqueRowValues));
-    const options = extraOptions.filter(opt => opt).concat(uniqueRowValues.map(value => ({ value, label: value })));
-    const query = this.props.query[column.property];
-    const value = query ? query.map(value => ({ value, label: value })) : [];
-
-    return (
-      <div className="column-filter-dropdown">
-        <Select
-          isMulti={true}
-          name={column.property}
-          value={value}
-          onChange={onQueryChange}
-          options={options}
-        />
-      </div>
-    );
-  }
-
-  evaluateExtraOption(column, uniqueRowValues, option) {
-    if (typeof option === 'function') {
-      const currentValue = this.props.query[column.property] || [];
-      return option(column, uniqueRowValues, currentValue);
-    }
-    return option;
   }
 }
 
--- src/components/Tickets/Table.js
+++ src/components/Tickets/Table.js
@@ -188,30 +188,12 @@ export default class TicketsTable extends React.Component {
       {this.props.userColumns.length === 0 && (
         <div className="alert alert-warning">You haven't selected any columns above. That's why you don't see any tickets.</div>
       )}
-
-        <Table.Provider
-          className="table table-striped table-bordered"
+        <SearchColumns
+          query={query}
           columns={visibleColumns}
-        >
-          <Table.Header>
-            <SearchColumns
-              query={query}
-              columns={visibleColumns}
-              onChange={this.search}
-              rows={rows}
-            />
-          </Table.Header>
-          <Table.Body rowKey="id" rows={paginated.rows} onRow={this.onBodyRow} />
-          <TableFooter columns={visibleColumns} rows={paginated.rows} />
-        </Table.Provider>
-        {paginated.amount > 1 && (
-          <Pagination
-            changePage={this.changePage}
-            paginated={paginated}
-            pagination={pagination}
-            topHtmlId={this.getHtmlId()}
-          />
-        )}
+          onChange={this.search}
+          rows={rows}
+        />
       </div>
     );
   }
