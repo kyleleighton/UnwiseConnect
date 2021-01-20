04d58451313f57742b040f21517f530e5a0c41a2
Kyle Leighton <kyle.leighton1@gmail.com>
2021-01-20 10:58:48 -0500

    Add tabs to the dispatch page to switch between dispatch screens

---
+++ src/components/Dispatch/CalendarDispatcher/index.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class CalendarDispatcher extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h3>Calendar</h3>
+      </div>
+    )
+  }
+}
+
+export default CalendarDispatcher;
--- src/components/Dispatch/index.js
+++ src/components/Dispatch/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TicketsDispatcher from './TicketsDispatcher';
 import ToggleProjects from '../Tickets/ToggleProjects';
+import CalendarDispatcher from './CalendarDispatcher';
 
 const Dispatch = () => {
+  const [activeTab, setActiveTab] = useState('calendar'); // for dev; will be tickets by default when the feature is done
+
   return (
     <div className="panel-uc panel panel-default">
       <div className="panel-uc__heading panel-heading clearfix">
@@ -11,7 +14,15 @@ const Dispatch = () => {
           <ToggleProjects />
         </div>
       </div>
-      <TicketsDispatcher />
+      <div className="dispatch-tabs">
+        <button className="btn btn-default" type="button" onClick={() => setActiveTab('tickets')}>Tickets</button>
+        <button className="btn btn-default" type="button" onClick={() => setActiveTab('calendar')}>Calendar</button>
+      </div>
+      {activeTab === 'tickets' ? (
+        <TicketsDispatcher />
+      ) : (
+        <CalendarDispatcher />
+      )}
     </div>
   )
 }
